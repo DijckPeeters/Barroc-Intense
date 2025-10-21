64ceb53523b77ea71f43f3babc99c4d9cf18407c
marsimallow <marcelaaguirre0815@gmail.com>
2025-10-21 12:11:04 +0200

    Created User model

---
+++ BarrocIntens/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarrocIntens.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Department { get; set; }
+
+        public User(string username, string password, string department)
+        {
+            Username = username;
+            Password = password;
+            Department = department;
+        }
+    }
+}
