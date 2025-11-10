7450e4ba16699a94a517035d754d0bf3d71195d4
marsimallow <marcelaaguirre0815@gmail.com>
2025-11-10 10:37:21 +0100

    Added dashboards xaml
    Added xaml pages for all the departments.

--- Barroc-Intense/Barroc-Intense/Pages/Dashboards/MaintenanceDashboardPage.xaml.cs
+++ Barroc-Intense/Barroc-Intense/Pages/Dashboards/MaintenanceDashboardPage.xaml.cs
@@ -16,14 +16,14 @@ using Windows.Foundation.Collections;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace BarrocIntens.Views.Dashboards
+namespace Barroc_Intense.Pages.Dashboards
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class ManagerDashboardPage : Page
+    public sealed partial class MaintenanceDashboardPage : Page
     {
-        public ManagerDashboardPage()
+        public MaintenanceDashboardPage()
         {
             InitializeComponent();
         }
