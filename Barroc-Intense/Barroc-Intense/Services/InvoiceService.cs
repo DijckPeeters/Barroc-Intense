6a98e8ee9b20aee78e77fd3dfcbb3fba94ba9690
marsimallow <marcelaaguirre0815@gmail.com>
2025-12-15 10:05:56 +0100

    Created Invoice Service
    The code to convert the offer into an invoice.

---
+++ Barroc-Intense/Barroc-Intense/Services/InvoiceService.cs
@@ -0,0 +1,39 @@
+﻿using Barroc_Intense.Data;
+using Barroc_Intense.Pages.Dashboards;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barroc_Intense.Services
+{
+    internal class InvoiceService
+    {
+        public static Barroc_Intense.Data.Invoice CreateInvoiceFromOffer(Offer offer)
+        {
+            var invoice = new Barroc_Intense.Data.Invoice
+            {
+                InvoiceNumber = $"INV-{offer.OfferNumber}",
+                Date = DateTime.Now,
+                CustomerName = offer.CustomerName,
+                CustomerAddress = offer.CustomerAddress,
+                Items = offer.Items?.Select(item => new InvoiceItem
+                {
+                    Product = item.Product,
+                    Quantity = item.Quantity,
+                }).ToList() ?? new List<InvoiceItem>()
+            };
+
+            foreach (var item in invoice.Items)
+            {
+                if (item?.Product != null)
+                {
+                    item.Product.Stock -= item.Quantity;
+                }
+            }
+
+            return invoice;
+        }
+    }
+}
