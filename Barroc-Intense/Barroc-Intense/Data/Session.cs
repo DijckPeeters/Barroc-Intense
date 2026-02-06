c0ae2c9874fe701b79fd609b95077f2085312b01
Dijck <d.j.peeters@icloud.com>
2026-02-06 15:40:45 +0100

    Added a backbutton and Logoutbutton to finance,inkoop and sales

---
+++ Barroc-Intense/Barroc-Intense/Data/Session.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barroc_Intense.Data
+{
+    public static class Session
+    {
+        public static string Username { get; set; }
+        public static bool IsLoggedIn { get; set; }
+    }
+}
